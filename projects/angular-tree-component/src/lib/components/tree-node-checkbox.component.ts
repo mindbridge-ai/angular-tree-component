561de10730e3b441415549b0b2908c30c8549295
Scott Hardy <scott.hardy@mindbridge.ai>
2020-09-03 19:48:18 -0400

    merge conflict

--- projects/angular-tree-component/src/lib/components/tree-node-checkbox.component.ts
+++ projects/angular-tree-component/src/lib/components/tree-node-checkbox.component.ts
@@ -1,8 +1,14 @@
-import { Component, Input, ViewEncapsulation, OnInit, OnChanges } from '@angular/core';
-import { TreeNode } from '../models/tree-node.model';
+import {
+  Component,
+  Input,
+  ViewEncapsulation,
+  OnInit,
+  OnChanges,
+} from "@angular/core";
+import { TreeNode } from "../models/tree-node.model";
 
 @Component({
-  selector: 'tree-node-checkbox',
+  selector: "tree-node-checkbox",
   encapsulation: ViewEncapsulation.None,
   styles: [],
   template: `
@@ -11,24 +17,22 @@ import { TreeNode } from '../models/tree-node.model';
         class="tree-node-checkbox"
         type="checkbox"
         (click)="node.mouseAction('checkboxClick', $event)"
-<<<<<<< HEAD:projects/angular-tree-component/src/lib/components/tree-node-checkbox.component.ts
-        [checked]="node.isSelected"
-        [indeterminate]="node.isPartiallySelected"
-      />
-=======
         [checked]="node.someChildrenSelected()"
         [indeterminate]="node.someChildrenSelected() && !node.allChildrenSelected()"/>
->>>>>>> 40d3018... Node state changes after testing and by passing isSelected property:lib/components/tree-node-checkbox.component.ts
     </ng-container>
-  `
+  `,
 })
 export class TreeNodeCheckboxComponent implements OnInit, OnChanges {
-  @Input() node: TreeNode;
+  @Input()
+  node: TreeNode;
 
   public ngOnInit(): void {
     /* Set on Query */
-    if (this.node.parent && this.node.options.lazySelect && this.node.parent.allChildrenSelected()) {
-        this.node.setIsSelected(this.node.parent.allChildrenSelected());
+    if (
+      this.node.parent && this.node.options.lazySelect &&
+      this.node.parent.allChildrenSelected()
+    ) {
+      this.node.setIsSelected(this.node.parent.allChildrenSelected());
     }
   }
 
@@ -38,5 +42,4 @@ export class TreeNodeCheckboxComponent implements OnInit, OnChanges {
       this.node = node.currentValue;
     }
   }
-
 }
