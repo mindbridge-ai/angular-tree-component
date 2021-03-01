5b9a12c1fa90fe53d7393191ed130b9b8eeaa6c9
Srilatha <srilatha.mamidi@mindbridge.ai>
2021-03-01 11:30:40 -0500

    Revert "change click action to select tree node"
    This reverts commit 8ff7bd9b8d49ed4fa5f9cdb644c23915c6a80073.

--- projects/angular-tree-component/src/lib/models/tree-options.model.ts
+++ projects/angular-tree-component/src/lib/models/tree-options.model.ts
@@ -10,39 +10,22 @@ export interface IActionHandler {
 }
 
 export const TREE_ACTIONS = {
-  TOGGLE_ACTIVE: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node && node.toggleActivated(),
-  TOGGLE_ACTIVE_MULTI: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node && node.toggleActivated(true),
-  TOGGLE_SELECTED: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node && node.toggleSelected(),
-  ACTIVATE: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node.setIsActive(true),
-  DEACTIVATE: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node.setIsActive(false),
-  SELECT: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node.setIsSelected(true, false),
-  DESELECT: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node.setIsSelected(false, false),
+  TOGGLE_ACTIVE: (tree: TreeModel, node: TreeNode, $event: any) => node && node.toggleActivated(),
+  TOGGLE_ACTIVE_MULTI: (tree: TreeModel, node: TreeNode, $event: any) => node && node.toggleActivated(true),
+  TOGGLE_SELECTED: (tree: TreeModel, node: TreeNode, $event: any) => node && node.toggleSelected(),
+  ACTIVATE: (tree: TreeModel, node: TreeNode, $event: any) => node.setIsActive(true),
+  DEACTIVATE: (tree: TreeModel, node: TreeNode, $event: any) => node.setIsActive(false),
+  SELECT: (tree: TreeModel, node: TreeNode, $event: any) => node.setIsSelected(true, false),
+  DESELECT: (tree: TreeModel, node: TreeNode, $event: any) => node.setIsSelected(false, false),
   FOCUS: (tree: TreeModel, node: TreeNode, $event: any) => node.focus(),
-  TOGGLE_EXPANDED: (tree: TreeModel, node: TreeNode, $event: any) =>
-    node.hasChildren && node.toggleExpanded(),
+  TOGGLE_EXPANDED: (tree: TreeModel, node: TreeNode, $event: any) => node.hasChildren && node.toggleExpanded(),
   EXPAND: (tree: TreeModel, node: TreeNode, $event: any) => node.expand(),
   COLLAPSE: (tree: TreeModel, node: TreeNode, $event: any) => node.collapse(),
-  DRILL_DOWN: (tree: TreeModel, node: TreeNode, $event: any) =>
-    tree.focusDrillDown(),
-  DRILL_UP: (tree: TreeModel, node: TreeNode, $event: any) =>
-    tree.focusDrillUp(),
-  NEXT_NODE: (tree: TreeModel, node: TreeNode, $event: any) =>
-    tree.focusNextNode(),
-  PREVIOUS_NODE: (tree: TreeModel, node: TreeNode, $event: any) =>
-    tree.focusPreviousNode(),
-  MOVE_NODE: (
-    tree: TreeModel,
-    node: TreeNode,
-    $event: any,
-    { from, to }: { from: any; to: any }
-  ) => {
+  DRILL_DOWN: (tree: TreeModel, node: TreeNode, $event: any) => tree.focusDrillDown(),
+  DRILL_UP: (tree: TreeModel, node: TreeNode, $event: any) => tree.focusDrillUp(),
+  NEXT_NODE: (tree: TreeModel, node: TreeNode, $event: any) =>  tree.focusNextNode(),
+  PREVIOUS_NODE: (tree: TreeModel, node: TreeNode, $event: any) =>  tree.focusPreviousNode(),
+  MOVE_NODE: (tree: TreeModel, node: TreeNode, $event: any, {from , to}: {from: any, to: any}) => {
     // default action assumes from = node, to = {parent, index}
     if ($event.ctrlKey) {
       tree.copyNode(from, to);
@@ -54,7 +37,7 @@ export const TREE_ACTIONS = {
 
 const defaultActionMapping: IActionMapping = {
   mouse: {
-    click: TREE_ACTIONS.TOGGLE_SELECTED,
+    click: TREE_ACTIONS.TOGGLE_ACTIVE,
     dblClick: null,
     contextMenu: null,
     expanderClick: TREE_ACTIONS.TOGGLE_EXPANDED,
@@ -73,102 +56,52 @@ const defaultActionMapping: IActionMapping = {
 
 export interface IActionMapping {
   mouse?: {
-    click?: IActionHandler;
-    dblClick?: IActionHandler;
-    contextMenu?: IActionHandler;
-    expanderClick?: IActionHandler;
-    checkboxClick?: IActionHandler;
-    dragStart?: IActionHandler;
-    drag?: IActionHandler;
-    dragEnd?: IActionHandler;
-    dragOver?: IActionHandler;
-    dragLeave?: IActionHandler;
-    dragEnter?: IActionHandler;
-    drop?: IActionHandler;
-    mouseOver?: IActionHandler;
-    mouseOut?: IActionHandler;
+    click?: IActionHandler,
+    dblClick?: IActionHandler,
+    contextMenu?: IActionHandler,
+    expanderClick?: IActionHandler,
+    checkboxClick?: IActionHandler,
+    dragStart?: IActionHandler,
+    drag?: IActionHandler,
+    dragEnd?: IActionHandler,
+    dragOver?: IActionHandler,
+    dragLeave?: IActionHandler,
+    dragEnter?: IActionHandler,
+    drop?: IActionHandler,
+    mouseOver?: IActionHandler,
+    mouseOut?: IActionHandler
   };
   keys?: {
-    [key: number]: IActionHandler;
+    [key: number]: IActionHandler
   };
 }
 
 export class TreeOptions {
-  get hasChildrenField(): string {
-    return this.options.hasChildrenField || 'hasChildren';
-  }
-  get childrenField(): string {
-    return this.options.childrenField || 'children';
-  }
-  get displayField(): string {
-    return this.options.displayField || 'name';
-  }
-  get idField(): string {
-    return this.options.idField || 'id';
-  }
-  get isExpandedField(): string {
-    return this.options.isExpandedField || 'isExpanded';
-  }
-  get getChildren(): any {
-    return this.options.getChildren;
-  }
-  get levelPadding(): number {
-    return this.options.levelPadding || 0;
-  }
-  get useVirtualScroll(): boolean {
-    return this.options.useVirtualScroll;
-  }
-  get animateExpand(): boolean {
-    return this.options.animateExpand;
-  }
-  get animateSpeed(): number {
-    return this.options.animateSpeed || 1;
-  }
-  get animateAcceleration(): number {
-    return this.options.animateAcceleration || 1.2;
-  }
-  get scrollOnActivate(): boolean {
-    return this.options.scrollOnActivate === undefined
-      ? true
-      : this.options.scrollOnActivate;
-  }
-  get rtl(): boolean {
-    return !!this.options.rtl;
-  }
-  get rootId(): any {
-    return this.options.rootId;
-  }
-  get useCheckbox(): boolean {
-    return this.options.useCheckbox;
-  }
-  get useTriState(): boolean {
-    return this.options.useTriState === undefined
-      ? true
-      : this.options.useTriState;
-  }
-  get scrollContainer(): HTMLElement {
-    return this.options.scrollContainer;
-  }
-  get allowDragoverStyling(): boolean {
-    return this.options.allowDragoverStyling === undefined
-      ? true
-      : this.options.allowDragoverStyling;
-  }
+  get hasChildrenField(): string { return this.options.hasChildrenField || 'hasChildren'; }
+  get childrenField(): string { return this.options.childrenField || 'children'; }
+  get displayField(): string { return this.options.displayField || 'name'; }
+  get idField(): string { return this.options.idField || 'id'; }
+  get isExpandedField(): string { return this.options.isExpandedField || 'isExpanded'; }
+  get getChildren(): any { return this.options.getChildren; }
+  get levelPadding(): number { return this.options.levelPadding || 0; }
+  get useVirtualScroll(): boolean { return this.options.useVirtualScroll; }
+  get animateExpand(): boolean { return this.options.animateExpand; }
+  get animateSpeed(): number { return this.options.animateSpeed || 1; }
+  get animateAcceleration(): number { return this.options.animateAcceleration || 1.2; }
+  get scrollOnActivate(): boolean { return this.options.scrollOnActivate === undefined ? true : this.options.scrollOnActivate; }
+  get rtl(): boolean { return !!this.options.rtl; }
+  get rootId(): any {return this.options.rootId; }
+  get useCheckbox(): boolean { return this.options.useCheckbox; }
+  get useTriState(): boolean { return this.options.useTriState === undefined ? true : this.options.useTriState; }
+  get scrollContainer(): HTMLElement { return this.options.scrollContainer; }
+  get allowDragoverStyling(): boolean { return this.options.allowDragoverStyling === undefined ? true : this.options.allowDragoverStyling; }
   actionMapping: IActionMapping;
 
   constructor(private options: ITreeOptions = {}) {
-    this.actionMapping = defaultsDeep(
-      {},
-      this.options.actionMapping,
-      defaultActionMapping
-    );
+    this.actionMapping = defaultsDeep({}, this.options.actionMapping, defaultActionMapping);
     if (options.rtl) {
-      this.actionMapping.keys[KEYS.RIGHT] =
-        <IActionHandler>get(options, ['actionMapping', 'keys', KEYS.RIGHT]) ||
-        TREE_ACTIONS.DRILL_UP;
-      this.actionMapping.keys[KEYS.LEFT] =
-        <IActionHandler>get(options, ['actionMapping', 'keys', KEYS.LEFT]) ||
-        TREE_ACTIONS.DRILL_DOWN;
+      this.actionMapping.keys[KEYS.RIGHT] = <IActionHandler>get(options, ['actionMapping', 'keys', KEYS.RIGHT]) || TREE_ACTIONS.DRILL_UP;
+      this.actionMapping.keys[KEYS.LEFT] = <IActionHandler>get(options, ['actionMapping', 'keys', KEYS.LEFT]) || TREE_ACTIONS.DRILL_DOWN;
     }
   }
 
@@ -183,10 +116,9 @@ export class TreeOptions {
   allowDrop(element, to, $event?): boolean {
     if (this.options.allowDrop instanceof Function) {
       return this.options.allowDrop(element, to, $event);
-    } else {
-      return this.options.allowDrop === undefined
-        ? true
-        : this.options.allowDrop;
+    }
+    else {
+      return this.options.allowDrop === undefined ? true : this.options.allowDrop;
     }
   }
 
@@ -214,18 +146,14 @@ export class TreeOptions {
     }
 
     // account for drop slots:
-    return nodeHeight + (node.index === 0 ? 2 : 1) * this.dropSlotHeight;
+    return nodeHeight + (node.index === 0 ?  2 : 1) * this.dropSlotHeight;
   }
 
   get dropSlotHeight(): number {
-    return isNumber(this.options.dropSlotHeight)
-      ? this.options.dropSlotHeight
-      : 2;
+    return isNumber(this.options.dropSlotHeight) ? this.options.dropSlotHeight : 2;
   }
 
   get lazySelect(): boolean {
-    return this.options.lazySelect !== undefined
-      ? this.options.lazySelect
-      : false;
+    return this.options.lazySelect !== undefined ? this.options.lazySelect : false;
   }
 }
